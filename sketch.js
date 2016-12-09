ad35d31187ab227fb9d3fd0e5b38acda3671323e
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-09 18:30:00 +0100

    create 2 new cells and delete the old one

--- sketch.js
+++ sketch.js
@@ -17,6 +17,8 @@ function mousePressed() {
   for (var i = cells.length-1; i >= 0; i--) {
     if (cells[i].clicked(mouseX, mouseY)) {
       cells.push(cells[i].mitosis());
+      cells.push(cells[i].mitosis());
+      cells.splice(i, 1);
     }
   }
 }
