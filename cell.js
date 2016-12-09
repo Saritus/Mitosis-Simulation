50ea0a285cb204086d8f7223d0fb5473ba76dc5f
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-09 18:01:58 +0100

    Add color to cell

--- cell.js
+++ cell.js
@@ -1,6 +1,7 @@
 function Cell() {
   this.pos = createVector(random(width), random(height))
-  this.r = 20;
+  this.r = 50;
+  this.c = color(random(255), random(255), random(255));
 
   this.move = function() {
     var velocity = p5.Vector.random2D();
@@ -8,6 +9,7 @@ function Cell() {
   }
 
   this.show = function() {
+    fill(this.c);
     ellipse(this.pos.x, this.pos.y, this.r, this.r)
   }
 }
